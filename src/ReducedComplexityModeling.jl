7bb7e5adec1ed43230f756a3ac19f7d8b2ddd28e
Rahul Manavalan <rahul.learns.physics@gmail.com>
2022-08-22 19:32:29 +0200

    Code has been rewritten for the newly designed pipeline

--- src/ReducedComplexityModeling.jl
+++ src/ReducedComplexityModeling.jl
@@ -1,13 +1,21 @@
 module ReducedComplexityModeling
 
     using GeometricIntegrators
+    using GeometricIntegrators:SolutionODE
     using ReducedBasisMethods
+    using GeometricEquations
+    using LinearAlgebra
+    using GeometricIntegratorsDiffEq:ODESolution,ODEProblem
 
-#-------------------------------------------------------
-    #TODO : Add exports
-#-------------------------------------------------------
-
-    include("TrainingData.jl")
-    include("Models.jl")
+    include("TrainingData/TrainingData.jl")
+    include("Models/Models.jl")
     include("TrainingProblem.jl")
+
+    export Array
+    export GeometricIntegratorData, GeometricIntegratorEnsembleData
+    export Dependent, Independent
+    export ReducedBasisModel
+    export POD,reduce!,compose
+    
+    export AutoEncoderModel
 end
