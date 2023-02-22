0b956f2631c1d52fb1017d0b15431de493e1d761
Rahul Manavalan <rahul.learns.physics@ gmail.com>
2023-02-22 16:18:59 +0100

    Update linear operator inference.

--- src/Models/OperatorInference.jl
+++ src/Models/OperatorInference.jl
@@ -9,64 +9,62 @@ struct LSTSQ <: AbstractOptimizer end
 struct Newton <: AbstractOptimizer end
 
 struct PlainOperatorInference <: AbstractOperatorInference
+    tinterval
     snapshots
     linearity
     operator
 
-    function PlainOperatorInference(snapshots)
-        new(snapshots,Linear())
+    function PlainOperatorInference(tinterval,snapshots)
+        new(tinterval,snapshots,Linear())
     end 
 
-    function PlainOperatorInference(snapshots,linearity)
-        new(snapshots,linearity)
+    function PlainOperatorInference(tinterval,snapshots,linearity)
+        new(tinterval,snapshots,linearity)
     end 
 end 
 
-function gradient(snapshots)
-
+# We use forward difference for estimating the gradient.
+# A more appropriate approximation should be using higher order Fornberg methods.
+function gradient(tinterval,snapshots)
+    @assert size(snapshots,2)-1 ==  length(tinterval)
+    sdt = snapshots[:,2:end] - snapshots[:,1:end-1]
+    for i=1:length(tinterval)
+        sdt[:,i] = sdt[:,i]./tinterval[i]
+    end 
+    return sdt
 end 
 
-function optimize!(snaps,vel,::LSTSQ)
+function optimize(snaps,vel,::LSTSQ)
     # Conventional inference using Least squares
     # Ax = b
     # xTAT=bT
     # AT = bT \ xT
     A = transpose(vel)\transpose(snaps)
-    tranpose(A)
+    return tranpose(A)
 end 
 
 # Is usually very expensive
-function optimize!(snaps,vel,::Netwon)
+function optimize(snaps,vel,::Netwon)
+    ninstances = size(vel,2)
+    # Loss function
+    loss = function(A)
+        l = 0
+        for i=1:ninstances
+            eval = A*snaps[:,i]
+            l += norm(eval-vel[:,i])
+        end 
+        return l
+    end 
     
+    # Setup optimization problem
+    nx,ny = size(vel,1),size(snaps,1)
+    A = rand(nx,ny)
+    Optim.optimize(loss,A,LBFGS())
+    return A
 end 
 
 function infer!(opinfer::PlainOperatorInference{Any,::Linear},opt::AbstractOptimizer)
-    vel = gradient(opinfer.snapshots)
-    snaps = opinfer.snapshots
-    opinfer.operator = optimize!(snaps,vel,opt)
-end 
-
-function kron(v::Vector) 
-    n = length(vec)
-    Z = zeros(n*n)
-    k = 1
-    for elem in vec
-        for elem2 in vec
-            X[k] = elem*elem2
-            k = k + 1
-        end 
-    end 
-    Z
+    vel = gradient(opinder.tinterval,opinfer.snapshots)
+    snaps = opinfer.snapshots[:,1:end-1]
+    opinfer.operator = optimize(snaps,vel,opt)
 end
-
-function infer!(opinfer::PlainOperatorInference{Any,::Quadratic},opt::AbstractOptimizer)
-    vel = gradient(opinfer.snapshots)
-    snaps = opinfer.snapshots
-    qsnaps = zeros(size(snaps,1)^2,size(snaps,2))
-    for i=1:size(snaps,2)
-        qsnaps[:,i] = kron(snaps[:,i])
-    end 
-    
-    # A x + B (x⊗x) = v
-    # 
-end 
