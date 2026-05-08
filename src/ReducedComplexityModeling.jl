f3d32580760983515041156ce3c597e0a90d25b6
benedict-96 <benedikt.brantner@icloud.com>
2026-05-08 10:46:27 +0200

    Moved `DataLoader` functionality to `ReducedComplexityModeling`.

--- src/ReducedComplexityModeling.jl
+++ src/ReducedComplexityModeling.jl
@@ -15,31 +15,43 @@ using AbstractNeuralNetworks: QPT, QPTOAT, Chain, NeuralNetworkParameters, Neura
 import ChainRulesCore
 using ChainRulesCore: @thunk, Thunk
 
-    export Array
-    export GeometricIntegratorData, GeometricIntegratorEnsembleData
-    export Dependent, Independent
-    export ReducedBasisModel
-    export POD,reduce!,project,compose
-    
-    export AutoEncoderModel
+export DataLoader
 
 # needed for shuffle function
 import Random
 
-    include("parameters/parameter.jl")
+include("data_loader/data_loader.jl")
+include("data_loader/tensor_assign.jl")
+include("data_loader/matrix_assign.jl")
+include("data_loader/mnist_utils.jl")
+include("data_loader/batch.jl")
 
-    export Parameter, hassamples
+include("TrainingData/TrainingData.jl")
+include("Models/Models.jl")
+include("TrainingProblem.jl")
 
-    include("parameters/parametersampler.jl")
+export Array
+export GeometricIntegratorData, GeometricIntegratorEnsembleData
+export Dependent, Independent
+export ReducedBasisModel
+export POD, reduce!, project, compose
 
-    export ParameterSampler, CartesianParameterSampler, sample
+export AutoEncoderModel
 
-    include("parameters/parameterspace.jl")
+include("parameters/parameter.jl")
 
-    export ParameterSpace
+export Parameter, hassamples
 
-    include("parameters/h5routines.jl")
+include("parameters/parametersampler.jl")
 
-    export h5save, h5load, read_sampling_parameters
+export ParameterSampler, CartesianParameterSampler, sample
+
+include("parameters/parameterspace.jl")
+
+export ParameterSpace
+
+include("parameters/h5routines.jl")
+
+export h5save, h5load, read_sampling_parameters
 
 end
